0b2a9cd7d74ba6f3467617bf10d29c9196b4a63d
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-12-12 16:51:06 +0100

    added pass validator

--- webPortal/src/app/login/login.component.ts
+++ webPortal/src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { Login } from 'src/shared/loginAuth.actions';
 import { Router } from '@angular/router';
 import { LoginAuthState } from 'src/shared/loginAuth.state';
 import * as zxcvbn from 'zxcvbn';
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,8 +15,14 @@ import * as zxcvbn from 'zxcvbn';
 export class LoginComponent implements OnInit {
 
   loginAuth = new LoginAuth();
+  passwordMessage = '';
 
   constructor(private store: Store, private router: Router) { }
+
+  loginForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl()
+  });
 
   ngOnInit() {
 
@@ -27,5 +34,15 @@ export class LoginComponent implements OnInit {
           this.router.navigateByUrl('');
       }
     });
+  }
+
+  passValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors => {
+      const passTest = zxcvbn(control.value);
+      const message = `Password strength ${passTest.score} / 4 - must be 3 or 4,
+      ${passTest.feedback.warning}`;
+      this.passwordMessage = message;
+      return passTest.score > 3 ? { weakPassword : message} : null;
+    };
   }
 }
