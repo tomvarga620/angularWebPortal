ac26bf72ad7c56279451939cae01e50c9f1203ec
Tomáš Varga <tomas.varga@akademiasovy.sk>
2020-01-23 18:30:12 +0100

    article component

--- webPortal/src/home/home.module.ts
+++ webPortal/src/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { ArctileWrapperComponent } from './arctile-wrapper/arctile-wrapper.component';
+import { ArticleComponent } from './article/article.component';
 
 
 @NgModule({
-  declarations: [ArctileWrapperComponent],
+  declarations: [ArctileWrapperComponent, ArticleComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
