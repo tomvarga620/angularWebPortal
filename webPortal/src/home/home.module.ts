046e0fc5db571b46cdcfdeb72a9f0799a8bbaeca
Adam966 <adam.ivan@akademiasovy.sk>
2020-01-24 09:34:30 +0100

    merge

--- webPortal/src/home/home.module.ts
+++ webPortal/src/home/home.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { ArticleComponent } from './article/article.component';
-import { ArticleDetailComponent } from './article-detail/article-detail.component';
 import { ArticlesHolderComponent } from './articles-holder/articles-holder.component';
 import { HomeMainComponent } from './home-main/home-main.component';
 import { ArticleHeaderComponent } from './article-header/article-header.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 
 @NgModule({
-  declarations: [ArticleComponent, ArticlesHolderComponent, HomeMainComponent, ArticleHeaderComponent],
+  declarations: [ArticleComponent, ArticlesHolderComponent, HomeMainComponent, ArticleHeaderComponent, ArticleDetailComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
   ]
 })
 export class HomeModule { }
