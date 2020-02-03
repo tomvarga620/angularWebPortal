2df9857e71f07a9cdd223a2adf3793f8d2253c43
Tomáš Varga <tomas.varga@akademiasovy.sk>
2020-02-03 18:46:29 +0100

    reactive form create article, added edit component

--- webPortal/src/app/create-article/create-article.component.ts
+++ webPortal/src/app/create-article/create-article.component.ts
@@ -3,22 +3,54 @@ import { UserServerService } from 'src/service.user-server/user-server.service';
 import { SnackbarService } from 'src/service.user-server/snackbar.service';
 import { Login } from 'src/shared/loginAuth.actions';
 import { Article } from '../entities/Article';
+import { FormGroup, FormControlName, FormControl, Validators, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-create-article',
   templateUrl: './create-article.component.html',
   styleUrls: ['./create-article.component.css']
 })
+
 export class CreateArticleComponent implements OnInit {
-  author = '';
+ /* author = '';
   title = '';
   category = '';
   description = '';
-  content = '';
+  content = '';*/
   file: File = null;
   data: FormData;
   isFile = false;
-  article:Article = new Article();
+  // article: Article = new Article();
+
+  articleForm = new FormGroup({
+
+    author: new FormControl('', Validators.min(4)),
+    title: new FormControl('', Validators.min(4)),
+    category: new FormControl('', Validators.min(4)),
+    description: new FormControl('', [Validators.min(3), Validators.max(60)]),
+    content: new FormControl('', [Validators.min(3), Validators.min(120)])
+
+  });
+
+  get author() {
+    return this.articleForm.get('author');
+  }
+
+  get title() {
+    return this.articleForm.get('title');
+  }
+
+  get category() {
+    return this.articleForm.get('category');
+  }
+
+  get description() {
+    return this.articleForm.get('description');
+  }
+
+  get content() {
+    return this.articleForm.get('content');
+  }
 
   constructor(private userServerService: UserServerService, private errorMessage: SnackbarService) { }
 
@@ -30,31 +62,45 @@ export class CreateArticleComponent implements OnInit {
       this.isFile = true;
       this.file = <File>event.target.files[0];
       this.data = new FormData();
-      this.data.append("file", this.file, this.file.name);
+      this.data.append('file', this.file, this.file.name);
       console.log(this.file.name);
-      
+
     } else {
-      this.errorMessage.errorMessage("Wrong type bro!")
+      this.errorMessage.errorMessage('Wrong type bro!')
     }
   }
 
   uploadFile() {
     this.userServerService.upLoadImage(this.data)
-      .subscribe(() => console.log("REQUEST"))
+      .subscribe(() => console.log('REQUEST'));
   }
 
   postArticle() {
-    this.article.author = this.author;
-    this.article.category = this.category;
+    /*
+    this.article.author = this.author.value;
+    this.article.category = this.category.value;
     this.article.content = this.content;
     this.article.date = new Date().toDateString();
     this.article.description = this.description;
     this.article.title = this.title;
     this.article.imgUrl = this.file.name;
-    console.log(this.article);
+    console.log(this.article);*/
+
+    const date = new Date().toDateString();
+
+    const article = new Article (
+      this.category.value,
+      this.title.value,
+      date,
+      //imageUrl
+      this.author.value,
+      this.description.value,
+      this.content.value
+    );
+
     this.uploadFile();
 
-    this.userServerService.postArticle(this.article)
+    this.userServerService.postArticle(article)
       .subscribe(() => console.log("POST ARTICLE"));
   }
 
--- webPortal/src/users/users.module.ts
+++ webPortal/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { UsersTableComponent } from './users-table/users-table.component';
+import { UsersEditComponent } from './users-edit/users-edit.component';
 
 @NgModule({
-  declarations: [UsersTableComponent],
+  declarations: [UsersTableComponent, UsersEditComponent],
   imports: [
   CommonModule,
     UsersRoutingModule,
